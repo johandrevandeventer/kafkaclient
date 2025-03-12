7e81047d052bf1ea94fd70df17391809bab9357a
johandrevandeventer <johandre.vandeventer@rubiconsa.com>
2025-03-12 10:28:23 +0200

    Add Prometheus server startup function to main package

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/johandrevandeventer/kafkaclient/config"
 	"github.com/johandrevandeventer/kafkaclient/consumer"
 	"github.com/johandrevandeventer/kafkaclient/producer"
+	prometheusserver "github.com/johandrevandeventer/kafkaclient/prometheus"
 	"go.uber.org/zap"
 )
 
@@ -15,4 +16,8 @@ func NewProducerPool(cfg *config.KafkaProducerConfig, logger *zap.Logger) (*prod
 
 func NewConsumer(ctx context.Context, cfg *config.KafkaConsumerConfig, logger *zap.Logger) (*consumer.KafkaConsumer, error) {
 	return consumer.NewKafkaConsumer(ctx, cfg, logger)
+}
+
+func StartPrometheus(address string) {
+	go prometheusserver.StartPrometheusServer(address)
 }
